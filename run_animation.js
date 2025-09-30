42f80128163688f70edcccbbe28e585314c7718f
Daniel Giger <danielg1111@verizon.net>
2025-09-29 23:57:36 -0400

    draw cube on top of canvas

--- run_animation.js
+++ run_animation.js
@@ -1,1 +1,18 @@
-console.log("yooo")
+
+canvas = document.getElementById("canvas");
+ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#66F"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+animate_cube();
+
+function animate_cube() {
+    img = new Image();
+    img.src = 'cube.png';
+    w = 100;
+    h = 100;
+    img.onload = function(){
+        ctx.drawImage(img, 0, 0, w, h);
+    }
+}
