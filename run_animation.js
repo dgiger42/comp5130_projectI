5ea0ad60d930f04c8529ec2797e86c24011ef438
Daniel Giger <danielg1111@verizon.net>
2025-09-30 00:42:39 -0400

    cube now has its own column

--- run_animation.js
+++ run_animation.js
@@ -3,6 +3,7 @@
 setInterval(animate_cube, 25)
 
 let cube_x = 0;
+let cube_y = 0;
 
 function animate_cube() {
     img_elem = document.getElementById("cube_img");
@@ -10,5 +11,9 @@ function animate_cube() {
 
     cube_x += 1;
     cube_x %= 200
+    cube_y += 1;
+    cube_y %= 400;
+
     img_elem.style.left = cube_x + "px";
+    img_elem.style.top = cube_y + "px"
 }
