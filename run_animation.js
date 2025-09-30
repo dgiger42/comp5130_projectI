7ceac8ef73a6cd14dce5d344ab1033d8de464de9
Daniel Giger <danielg1111@verizon.net>
2025-09-30 01:06:02 -0400

    make animate_cube() more more flexible, so it can later do > 1 cube

--- run_animation.js
+++ run_animation.js
@@ -2,18 +2,20 @@
 
 setInterval(animate_cube, 25)
 
-let cube_x = 0;
-let cube_y = 0;
-
 function animate_cube() {
     img_elem = document.getElementById("cube_img");
     // console.log(img_elem);
 
+    style = img_elem.style;
+
+    cube_x = parseInt(style.left.slice(0, -2))
+    cube_y = parseInt(style.top.slice(0, -2))
     cube_x += 1;
     cube_x %= 200
     cube_y += 1;
     cube_y %= 400;
 
-    img_elem.style.left = cube_x + "px";
-    img_elem.style.top = cube_y + "px"
+
+    style.left = cube_x + "px";
+    style.top = cube_y + "px"    
 }
