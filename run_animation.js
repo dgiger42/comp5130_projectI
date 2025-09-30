7f081fb67982c4c50db4fc6c1872a89475e11d5c
Daniel Giger <danielg1111@verizon.net>
2025-09-30 01:45:18 -0400

    white text black background

--- run_animation.js
+++ run_animation.js
@@ -1,22 +1,38 @@
 
-setInterval(animate_cube, 25, "cube1")
-setInterval(animate_cube, 25, "cube2")
+interval = 25
+setInterval(animate_cube, interval, "cube1")
+setInterval(animate_cube, interval, "cube2")
 
 function animate_cube(cube_id) {
     img_elem = document.getElementById(cube_id);
+    parent = img_elem.parentNode;
+
+    parent_rect = parent.getBoundingClientRect();
+    img_rect = img_elem.getBoundingClientRect();
+    
+    max_x_offset = parent_rect.width - img_rect.width;
+    // max_y_offset = parent_rect.height - img_rect.height;
+    max_y_offset = 400;
+    console.log(parent_rect)
+
     // console.log(img_elem);
 
+
     style = img_elem.style;
-    if (cube_id == "cube1") {
-        console.log(style.left)
-    }
+
+
+
+    // rect = img_elem.getBoundingClientRect()
+    // if (cube_id == "cube1") {
+    //     console.log(rect)
+    // }
 
     cube_x = parseInt(style.left.slice(0, -2))
     cube_y = parseInt(style.top.slice(0, -2))
     cube_x += 1;
-    cube_x %= 200
+    cube_x %= max_x_offset;
     cube_y += 1;
-    cube_y %= 400;
+    cube_y %= max_y_offset;
 
 
     style.left = cube_x + "px";
