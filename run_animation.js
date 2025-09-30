df252429913c3ed27ac66121b825c698d12b2efa
Daniel Giger <danielg1111@verizon.net>
2025-09-30 00:04:41 -0400

    start using setInterval

--- run_animation.js
+++ run_animation.js
@@ -5,14 +5,22 @@ ctx = canvas.getContext("2d");
 ctx.fillStyle = "#66F"
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-animate_cube();
 
-function animate_cube() {
-    img = new Image();
-    img.src = 'cube.png';
-    w = 100;
-    h = 100;
-    img.onload = function(){
-        ctx.drawImage(img, 0, 0, w, h);
-    }
+setInterval(a_callback, 500)
+
+function a_callback() {
+    console.log("hola")
 }
+
+// animate_cube();
+
+// function animate_cube() {
+//     img = new Image();
+//     img.src = 'cube.png';
+//     w = 100;
+//     h = 100;
+//     img.onload = function(){
+//         ctx.drawImage(img, 0, 0, w, h);
+//         console.log(img)
+//     }
+// }
