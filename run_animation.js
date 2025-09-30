b67c41bd761e06bb341f28140209f2fc9337d1c6
Daniel Giger <danielg1111@verizon.net>
2025-09-30 00:32:04 -0400

    get rid of the canvas

--- run_animation.js
+++ run_animation.js
@@ -1,16 +1,10 @@
 
-canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#66F"
-ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-
-setInterval(a_callback, 25)
+setInterval(animate_cube, 25)
 
 let cube_x = 0;
 
-function a_callback() {
+function animate_cube() {
     img_elem = document.getElementById("cube_img");
     // console.log(img_elem);
 
@@ -18,16 +12,3 @@ function a_callback() {
     cube_x %= 200
     img_elem.style.left = cube_x + "px";
 }
-
-// animate_cube();
-
-// function animate_cube() {
-//     img = new Image();
-//     img.src = 'cube.png';
-//     w = 100;
-//     h = 100;
-//     img.onload = function(){
-//         ctx.drawImage(img, 0, 0, w, h);
-//         console.log(img)
-//     }
-// }
